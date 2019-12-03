839a3306faaecce86cd3cb336d2cbe1d50eebf6c
Luca <lucaantinori86@gmail.com>
2019-12-03 17:14:01 +0100

    2 data binding + info page

--- app/Map/Map-view-model.js
+++ app/Map/Map-view-model.js
@@ -3,6 +3,8 @@ const observableModule = require("tns-core-modules/data/observable");
 function SearchViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        nome:'',
+        cognome:''
     });
 
     return viewModel;
