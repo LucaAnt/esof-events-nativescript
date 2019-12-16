d00aa9794a1e535dc9435afb1f66d72490688003
Luca <lucaantinori86@gmail.com>
2019-12-16 15:22:33 +0100

    add navigation function

--- app/Information/information-page.js
+++ app/Information/information-page.js
@@ -5,4 +5,13 @@ function onNavigatingTo(args) {
     component.bindingContext = new SearchViewModel();
 }
 
+function onTap(args)
+{
+    const button = args.object;
+    const page = button.page;
+    page.frame.navigate("Map/Map-page");
+    console.log('FIRE !!!!');
+}
+
 exports.onNavigatingTo = onNavigatingTo;
+exports.onTap = onTap;
